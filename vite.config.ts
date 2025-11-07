770a8c8d7b2624b80e8757c994ed3bc2ea24777d
Yugananthan P <yugananthanpalani@outlook.com>
2025-11-07 14:43:33 +0530

    env commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,8 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    host: true,      // allows access from network (e.g., via phone or other device)
+    port: 5173,      // change this to your preferred port (e.g., 3000)
+  },
 });
